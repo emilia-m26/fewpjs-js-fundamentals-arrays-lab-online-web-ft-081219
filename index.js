cfdd1e8dee90de620b19de71ca2b0fc50794b7ac
Emilia <emilia.mena26@gmail.com>
2019-12-07 12:03:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your solution here!
-const append = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"]; //assigns infor to variable
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
-append.push('Odie')
+append.push('Odie') //uses variable to make changes
 prepend.unshift('Odie')
 removeLast.pop()
 removeFirst.shift()
